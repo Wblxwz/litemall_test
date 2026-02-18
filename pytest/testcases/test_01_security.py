9ae6fb8de40e496fffc0fc947cd56c8c96c9fffc
wblxwz <2394559659@qq.com>
2026-02-19 00:23:26 +0800

    add login fail

--- pytest/testcases/test_01_security.py
+++ pytest/testcases/test_01_security.py
@@ -16,13 +16,39 @@ class TestSecurity:
     @allure.testcase("testlink")
     def test_login(self):
         api = common.get_json("url") + common.get_json("login_api")
-        response = common.login(api,common.get_json("default_username"),common.get_json("default_username"))
+        response = common.login(api,common.get_json("default_username"),common.get_json("default_password"))
         my_logger.info(response.text)
         json_data = json.loads(response.text)
         errno = json_data["errno"]
         errmsg = json_data["errmsg"]
         assert  errno == 0
         assert errmsg == "成功"
+    @allure.story("登录功能")
+    @allure.description("使用错误用户名登录失败")
+    @allure.issue("jira")
+    @allure.testcase("testlink")
+    def test_login_fail_username(self):
+        api = common.get_json("url") + common.get_json("login_api")
+        response = common.login(api,common.get_json("wrong_username"),common.get_json("default_password"))
+        my_logger.info(response.text)
+        json_data = json.loads(response.text)
+        errno = json_data["errno"]
+        errmsg = json_data["errmsg"]
+        assert  errno == 700
+        assert errmsg == "账号不存在"
+    @allure.story("登录功能")
+    @allure.description("使用错误密码登录失败")
+    @allure.issue("jira")
+    @allure.testcase("testlink")
+    def test_login_fail_password(self):
+        api = common.get_json("url") + common.get_json("login_api")
+        response = common.login(api,common.get_json("default_username"),common.get_json("wrong_password"))
+        my_logger.info(response.text)
+        json_data = json.loads(response.text)
+        errno = json_data["errno"]
+        errmsg = json_data["errmsg"]
+        assert  errno == 700
+        assert errmsg == "账号密码不对"
 
 if __name__ == "__main__":
     test = TestSecurity()
